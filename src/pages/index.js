4e68f811299ab985a3b06181d8d9fbc272336155
Anoctis <65804464+Anoctis@users.noreply.github.com>
2023-02-26 18:29:58 +0500

    check

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,6 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold underline">
-      taha
-    </h1>
+      Testing change   </h1>
   )
 }
