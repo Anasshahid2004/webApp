e6372335568737bee458500cb00b8fae4f40eb88
Anasshahid2004 <anas.shahid.990@gmail.com>
2023-02-26 18:12:11 +0500

    name change

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold underline">
-      ahsan
+      taha
     </h1>
   )
 }
