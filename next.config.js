2a071239adecdcb1c66b937a63d413d1548b8f59
Anasshahid2004 <anas.shahid.990@gmail.com>
2023-02-26 19:14:31 +0500

    Nav/Anas

--- next.config.js
+++ next.config.js
@@ -3,4 +3,21 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+// module.exports = {
+//   images: {
+//     domains: ['images.unsplash.com'],
+// },
+// }
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '', 
+       
+      },
+    ],
+  },
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,17 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      taha
-    </h1>
+    <>
+    <nav className='p-5 bg-white shadow'>
+    <div>
+       <span className='text-2xl font-[Poppins]'>
+       neuralnine
+       <Image className='' width={150} height={100} src='https://images.unsplash.com/photo-1677247191557-4abd28b7c387?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80'></Image>
+       </span>
+    </div>
+    </nav>
+      
+      </>
+    
   )
 }
