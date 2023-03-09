5a7d1af720f29f989345e078ed984c0da68e54cf
Anoctis <65804464+Anoctis@users.noreply.github.com>
2023-03-09 16:49:37 +0500

    Added Blog* Category Pages.

--- src/pages/finance.js
+++ src/pages/finance.js
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <>
     <Head>
-     <title>Machine Learning - NeuralNine</title>
+     <title>Finance - NeuralNine</title>
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
     </Head>
     <nav>
@@ -48,35 +48,28 @@ export default function Home() {
     <section className="bg-[#0e0e0e] text-gray-100">
         <div className="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-16 md:px-10 lg:px-32 xl:max-w-4xl">
         <h1 className="text-4xl font-bold leading-none sm:text-4xl">
-            <span className="text-[#FF7825]"> MACHINE LEARNING </span> 
+            <span className="text-[#FF7825]"> FINANCE </span> 
         </h1>
         <hr class="w-full h-0.5 mx-auto  bg-[#FF7825] border-0 rounded md:my-10"/>
-        <h1 className="text-3xl font-bold leading-none sm:text-2xl">PYTHON</h1>
+        <h1 className="text-2xl font-bold leading-none sm:text-2xl">PROGRAMMING</h1>
         </div>
         </section>
         <section className="bg-[#0e0e0e] text-gray-100 pb-20">
 	    <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
 		
-		<div className="grid justify-center grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
-            <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-black hidden sm:block">
-                <Image alt="presentation" width={434} height={434} className="object-cover w-full rounded h-100 bg-gray-500" src="https://media.discordapp.net/attachments/807562739324157962/1082969833009856552/tensorflowhandwritten.png?width=434&height=434" />
+		<div className="grid justify-center grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-2">
+        <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-black hidden sm:block">
+                    <Image alt="presentation" width={434} height={434} className="object-cover w-full rounded h-100 bg-gray-500" src="https://media.discordapp.net/attachments/807562739324157962/1082969833827745832/portthumb.png?width=434&height=434" />
                     <div className="p-4 space-y-2">
-                        <h3 className="text-[17px] font-[700] group-hover:underline group-focus:underline text-[#FF7825]">HANDWRITTEN DIGITS RECOGNITION WITH TENSORFLOW</h3>
-                        <p className='text-[15px]'>Introduction One of the most powerful and most popular libraries for machine learning out there is Tensorflow. It allows us to easily build, train and...</p>
+                        <h3 className="text-[17px] font-[700] group-hover:underline group-focus:underline text-[#FF7825]">PORTFOLIO VISUALIZER WITH MATPLOTLIB IN PYTHON</h3>
+                        <p className='text-[15px]'>Introduction Python is the number one language for machine learning and data science right now. For the most part this is because of the powerful....</p>
                     </div>
                 </a>
                 <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-black hidden sm:block">
-                    <Image alt="presentation" width={434} height={434} className="object-cover w-full rounded h-100 bg-gray-500" src="https://media.discordapp.net/attachments/807562739324157962/1082969834142322751/kneighborsthumb.png?width=434&height=434" />
+                    <Image alt="presentation" width={434} height={434} className="object-cover w-full rounded h-100 bg-gray-500" src="https://media.discordapp.net/attachments/807562739324157962/1082969801179275324/candlestickpython.png?width=434&height=434" />
                     <div className="p-4 space-y-2">
-                        <h3 className="text-[17px] font-[700] group-hover:underline group-focus:underline text-[#FF7825]">K-NEAREST NEIGHBORS CLASSIFICATION FROM SCRATCH IN PYTHON</h3>
-                        <p className='text-[15px]'>Introduction Classification is a supervised learning process that categorizes data into classes. One of the most popular classification algorithms in machine learning is the K-Nearest Neighbors....</p>
-                    </div>
-                </a>
-                <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-black hidden sm:block">
-                    <Image alt="presentation" width={434} height={434} className="object-cover w-full rounded h-100 bg-gray-500" src="https://media.discordapp.net/attachments/807562739324157962/1082969801544171540/linear_regression.png?width=434&height=434" />
-                    <div className="p-4 space-y-2">
-                        <h3 className="text-[17px] font-[700] group-hover:underline group-focus:underline text-[#FF7825]">LINEAR REGRESSION FROM SCRATCH IN PYTHON</h3>
-                        <p className='text-[15px]'>Introduction One of the most basic machine learning models is the Linear Regression. The purpose of linear regression is to take a bunch of data....</p>
+                        <h3 className="text-[17px] font-[700] group-hover:underline group-focus:underline text-[#FF7825]">PLOT CANDLESTICK CHARTS IN PYTHON</h3>
+                        <p className='text-[15px]'>Introduction Candlestick charts are one of the best ways to visualize stock data because they give us very detailed information about the evolution of share...</p>
                     </div>
                 </a>
 		</div>
--- src/pages/other.js
+++ src/pages/other.js
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <>
     <Head>
-     <title>Machine Learning - NeuralNine</title>
+     <title>Other - NeuralNine</title>
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
     </Head>
     <nav>
@@ -48,37 +48,30 @@ export default function Home() {
     <section className="bg-[#0e0e0e] text-gray-100">
         <div className="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-16 md:px-10 lg:px-32 xl:max-w-4xl">
         <h1 className="text-4xl font-bold leading-none sm:text-4xl">
-            <span className="text-[#FF7825]"> MACHINE LEARNING </span> 
+            <span className="text-[#FF7825]"> OTHER </span> 
         </h1>
         <hr class="w-full h-0.5 mx-auto  bg-[#FF7825] border-0 rounded md:my-10"/>
-        <h1 className="text-3xl font-bold leading-none sm:text-2xl">PYTHON</h1>
+        <h1 className="text-2xl font-bold leading-none sm:text-2xl">RANKINGS & IDEAS</h1>
         </div>
         </section>
         <section className="bg-[#0e0e0e] text-gray-100 pb-20">
 	    <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
 		
-		<div className="grid justify-center grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
+		<div className="grid justify-center grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-2">
             <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-black hidden sm:block">
-                <Image alt="presentation" width={434} height={434} className="object-cover w-full rounded h-100 bg-gray-500" src="https://media.discordapp.net/attachments/807562739324157962/1082969833009856552/tensorflowhandwritten.png?width=434&height=434" />
-                    <div className="p-4 space-y-2">
-                        <h3 className="text-[17px] font-[700] group-hover:underline group-focus:underline text-[#FF7825]">HANDWRITTEN DIGITS RECOGNITION WITH TENSORFLOW</h3>
-                        <p className='text-[15px]'>Introduction One of the most powerful and most popular libraries for machine learning out there is Tensorflow. It allows us to easily build, train and...</p>
-                    </div>
+                <Image alt="presentation" width={434} height={434} className="object-cover w-full rounded h-100 bg-gray-500" src="https://media.discordapp.net/attachments/807562739324157962/1082969802664058961/50ideas.png?width=434&height=434" />
+                <div className="p-4 space-y-2">
+                    <h3 className="text-[17px] font-[700] group-hover:underline group-focus:underline text-[#FF7825]">50+ PYTHON PROGRAMMING IDEAS</h3>
+                    <p className='text-[15px]'>Introduction Programming is fun. It enables us to automate processes and create awesome applications. But just reading books and watching tutorials is not enough. We....</p>
+                </div>
                 </a>
                 <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-black hidden sm:block">
-                    <Image alt="presentation" width={434} height={434} className="object-cover w-full rounded h-100 bg-gray-500" src="https://media.discordapp.net/attachments/807562739324157962/1082969834142322751/kneighborsthumb.png?width=434&height=434" />
-                    <div className="p-4 space-y-2">
-                        <h3 className="text-[17px] font-[700] group-hover:underline group-focus:underline text-[#FF7825]">K-NEAREST NEIGHBORS CLASSIFICATION FROM SCRATCH IN PYTHON</h3>
-                        <p className='text-[15px]'>Introduction Classification is a supervised learning process that categorizes data into classes. One of the most popular classification algorithms in machine learning is the K-Nearest Neighbors....</p>
-                    </div>
-                </a>
-                <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-black hidden sm:block">
-                    <Image alt="presentation" width={434} height={434} className="object-cover w-full rounded h-100 bg-gray-500" src="https://media.discordapp.net/attachments/807562739324157962/1082969801544171540/linear_regression.png?width=434&height=434" />
-                    <div className="p-4 space-y-2">
-                        <h3 className="text-[17px] font-[700] group-hover:underline group-focus:underline text-[#FF7825]">LINEAR REGRESSION FROM SCRATCH IN PYTHON</h3>
-                        <p className='text-[15px]'>Introduction One of the most basic machine learning models is the Linear Regression. The purpose of linear regression is to take a bunch of data....</p>
-                    </div>
-                </a>
+                <Image alt="presentation" width={434} height={434} className="object-cover w-full rounded h-100 bg-gray-500" src="https://media.discordapp.net/attachments/807562739324157962/1082969800843722802/top5.png?width=434&height=434" />
+                <div className="p-4 space-y-2">
+                    <h3 className="text-[17px] font-[700] group-hover:underline group-focus:underline text-[#FF7825]">TOP 5 PROGRAMMING LANGUAGES FOR 2019</h3>
+                    <p className='text-[15px]'>Introduction Right now, there are hundreds, if not thousands, of different programming languages out there. Some of them are used quite frequently and some of....</p>
+                </div>
+            </a>
 		</div>
 	  </div>
     </section>
