15c5a4911f2c0cccb821b2b0c30759a3c69cd830
Anoctis <65804464+Anoctis@users.noreply.github.com>
2023-03-15 16:16:29 +0500

    Navbar Updated.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,84 +1,140 @@
-import React from "react";
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
 
-const NavBar = () => {
+function Navbar(props) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <>
-      <nav className="bg-black shadow md:flex md:items-center md:justify-evenly list-none px-6">
-        <div className="flex justify-between items-center">
-          <Link href="/">
-            <Image
-              className="inline w-[22%]"
-              width={1024}
-              height={774}
-              alt="img"
-              src="https://media.discordapp.net/attachments/807562739324157962/1080232878887161896/logo.png?width=1025&height=425"
-            ></Image>
+    <nav className="bg-black pb-4 pt-3 sticky top-0 z-10">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-start ">
+            <Link
+              href={"/"}
+              className="flex title-font font-medium items-center text-white mb-4 md:mb-0"
+            >
+              <Image
+                className=" w-[24%] "
+                width={1024}
+                height={774}
+                alt="img"
+                src="https://media.discordapp.net/attachments/807562739324157962/1080232878887161896/logo.png?width=1025&height=425"
+              ></Image>
+            </Link>
+          </div>
+          <div className="-mr-2 flex sm:hidden ">
+            <button
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <svg
+                className="block h-6 w-6"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d={
+                    isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"
+                  }
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="hidden  sm:flex sm:items-center ">
+            <Link
+              href={"/"}
+              className="mr-5 text-white mt-3 hover:text-orange-400 text-lg cursor-pointer px-4 "
+            >
+              Home
+            </Link>
+            <Link
+              href={"/about"}
+              className="mr-5 text-white mt-3 hover:text-orange-400 text-lg cursor-pointer px-4"
+            >
+              About
+            </Link>
+            <Link
+              href={"/blog"}
+              className="mr-5 text-white mt-3 hover:text-orange-400 text-lg cursor-pointer px-4"
+            >
+              Blog
+            </Link>
+            <Link
+              href={"/books"}
+              className="mr-5 text-white mt-3 hover:text-orange-400 text-lg cursor-pointer px-4"
+            >
+              Books
+            </Link>
+            <Link
+              href={"/videos"}
+              className="mr-5 text-white mt-3 hover:text-orange-400 text-lg cursor-pointer px-4"
+            >
+              Videos
+            </Link>
+            <Link
+              href={"/shop"}
+              className="mr-5 text-white mt-3 hover:text-orange-400 text-lg cursor-pointer px-4"
+            >
+              Shop
+            </Link>
+            <Link
+              href={"/setup"}
+              className="mr-5 text-white mt-3 hover:text-orange-400 text-lg cursor-pointer px-4"
+            >
+              Setup
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={`${isOpen ? "block" : "hidden"} sm:hidden `}>
+        <div className="px-2 pt-2 pb-3">
+          <Link
+            href={"/"}
+            className="mr-5 block my-2 text-white hover:text-orange-400 text-lg cursor-pointer px-4"
+          >
+            Home
+          </Link>
+          <Link
+            href={"/about"}
+            className="mr-5 block my-2 text-white hover:text-orange-400 text-lg cursor-pointer px-4"
+          >
+            About
+          </Link><Link
+            href={"/blog"}
+            className="mr-5 block my-2 text-white hover:text-orange-400 text-lg cursor-pointer px-4"
+          >
+            Blog
+          </Link><Link
+            href={"/books"}
+            className="mr-5 block my-2 text-white hover:text-orange-400 text-lg cursor-pointer px-4"
+          >
+            Books
+          </Link><Link
+            href={"/videos"}
+            className="mr-5 block my-2 text-white hover:text-orange-400 text-lg cursor-pointer px-4"
+          >
+            Videos
+          </Link>
+          <Link
+            href={"/shop"}
+            className="mr-5 block my-2 text-white hover:text-orange-400 text-lg cursor-pointer px-4"
+          >
+            Shop
+          </Link>
+          <Link
+            href={"/setup"}
+            className="mr-5 block my-2 text-white hover:text-orange-400 text-lg cursor-pointer px-4"
+          >
+            Setup
           </Link>
         </div>
-
-        <ul className="md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-black w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 text-white">
-          <li className="mx-4 my-6 md:my-0 ">
-            <Link
-              href="/"
-              className="text-lg hover:text-orange-500 duration-500"
-            >
-              HOME
-            </Link>
-          </li>
-          <li className="mx-4 my-6 md:my-0 ">
-            <Link
-              href="/blog"
-              className="text-lg hover:text-orange-500 duration-500"
-            >
-              BLOG
-            </Link>
-          </li>
-          <li className="mx-4 my-6 md:my-0 ">
-            <Link
-              href="/books"
-              className="text-lg hover:text-orange-500 duration-500"
-            >
-              BOOKS
-            </Link>
-          </li>
-          <li className="mx-4 my-6 md:my-0 ">
-            <Link
-              href="/videos"
-              className="text-lg hover:text-orange-500 duration-500"
-            >
-              VIDEOS
-            </Link>
-          </li>
-          <li className="mx-4 my-6 md:my-0 ">
-            <Link
-              href="/shop"
-              className="text-lg hover:text-orange-500 duration-500"
-            >
-              SHOP
-            </Link>
-          </li>
-          <li className="mx-4 my-6 md:my-0 ">
-            <Link
-              href="/about"
-              className="text-lg hover:text-orange-500 duration-500"
-            >
-              ABOUT
-            </Link>
-          </li>
-          <li className="mx-4 my-6 md:my-0 ">
-            <Link
-              href="/setup"
-              className="text-lg hover:text-orange-500 duration-500"
-            >
-              SETUP
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
-};
+}
 
-export default NavBar;
+export default Navbar;
